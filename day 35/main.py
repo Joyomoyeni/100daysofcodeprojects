4a97a865962a4ff8fc33a51aa29abd5436836b12
Joyomoyeni <150476914+Joyomoyeni@users.noreply.github.com>
2024-08-29 10:14:56 +0100

    Update main.py

--- day 35/main.py
+++ day 35/main.py
@@ -1,12 +1,12 @@
-API_KEY = "484a4353d32a2291f4a61d55c447e920"
+API_KEY = ""
 long = 3.503440
 lat = 6.621070
 import requests
 from twilio.rest import Client
 
-TWILLIOCODE = "62XM55PYRR3K2V1WPBN48UBD"
-accountid = "AC867515e1a06ed821ea040195cd3a6e3c"
-accttoken = "f78f02c72c0c85c5dee93615202b11a7"
+TWILLIOCODE = ""
+accountid = ""
+accttoken = ""
 response = requests.get(
     f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={long}&cnt={4}&appid={API_KEY}")
 data = response.json()
